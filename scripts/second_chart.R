4f548b0384e93398c899a71fe3bd336bdf40d799
Yutian Pan <yupan@uw.edu>
2020-11-13 10:32:54 -0700

    basic plot completed
    not completed
    need adjustment on colors

--- scripts/second_chart.R
+++ scripts/second_chart.R
@@ -1,30 +1,40 @@
-#main product listing page 
-#subsequent listing page 
-
-#items lacked a shipping price 
-#(sold by amazon or sellers in "fullfilled by amazon")
-
-# 1st ranking, just the price (without shipping)
-# 2nd ranking, re-ranking after adding shipping. 
-
-# buy box (BB)
-# pop up first as a suggesed purchase 
-# 3/4 amazon own product / seller "fullfilled by amazon"
-# even when much cheaper options. 
-
-# amazon's giving ranking: 
-# all vendors  price + shipping
-# BUT own/FBA product OMIT shipping when ranking
-
-# To: show the different the price of one product (scraped/corrected/ )?
-
-# To: highest price - lowest price (box-plot)? 
-
-
-
 # How likely BBCorrectedPrice is (not) the lower price, based on different vendor type?
-
 # Faceted histogram:
-# three categories by “ScrapedIndexVendortype” : Amazon, FBA, Other. 
+# three categories by vendor type : Amazon, FBA, Other. (“ScrapedIndexVendortype”)
 # showing the distribution of “CorrectedPrice” / ”BBCorrectedPrice” ratio of all vendor listings.
 # If “CorrectedPrice”/”BBCorrectedPrice” Ratio < 1, BBCorrectedPrice is not the lower price in the listing.
+
+# Load the tidyverse package
+library("tidyverse")
+
+# Load Amazon 250 best selling products data from data downloaded in Github
+amazon_data <- read.csv(
+  paste("https://raw.githubusercontent.com/info201b-au20/project-",
+        "AmazonPricing/gh-pages/data/Amazon-Ranking-Analysis.csv?token=",
+        "AKQ3B6Q3UEQDBPM4IBQS3B27W2R5W",
+        sep = ""
+  )
+)
+
+# calculate the ratio of corrected price to Buy Box corrected price
+corrected_price_to_BBs_ratios <- amazon_data %>%
+  mutate(CorrectedToBBCorrectedRatio = amazon_data$CorrectedPrice / amazon_data$BBCorrectedPrice) %>%
+  select(Index, CorrectedPrice, BBCorrectedPrice, CorrectedToBBCorrectedRatio, ScrapedIndexVendorType, BBVendorType)
+
+# create the Faceted histogram
+ggplot(data = corrected_price_to_BBs_ratios) +
+  geom_point(mapping = aes(x = Index , y = CorrectedToBBCorrectedRatio)) +
+  facet_wrap(~ScrapedIndexVendorType) +
+  ggtitle(paste("Disstribution of ",
+                sep = ""
+  )) +
+  xlab("Scraped Index of Products") +
+  ylab("Ratio")
+
+# todo: color dots : == 1 / >1 / <1 
+
+# Simple Summary:
+# the chart suggests that most of BB product price is the same as amazon's own product's.
+# so BB product is more likely to be amazon's product?
+# FBA's price is trying to align with BBprice. or above.
+# but looking at other vendors', they would have many prices that's lower than BBprice.
