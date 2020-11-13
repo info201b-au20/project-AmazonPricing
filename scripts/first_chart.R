8445bfb3229f5990c042171272cfba96a6ffdf2d
Sydney <smiths12@uw.edu>
2020-11-13 07:56:15 -0800

    Completed Chart 2

---
+++ scripts/first_chart.R
@@ -0,0 +1,28 @@
+library(tidyverse)
+
+# Load the data from GitHub
+
+amazon_data <- read.csv(
+  paste("https://raw.githubusercontent.com/info201b-au20/project-",
+    "AmazonPricing/gh-pages/data/Amazon-Ranking-Analysis.csv?token=",
+    "AKQ3B6Q3UEQDBPM4IBQS3B27W2R5W",
+    sep = ""
+  )
+)
+
+# Check the number of total unique products is 250
+check <- amazon_data %>%
+  group_by(ProductName) %>%
+  count(unique(ProductName))
+
+# We will be examining the top 5 unique products which means the top 272 Indexes
+top_5_products <- amazon_data %>%
+  filter(Index <= 44)
+
+# Create a dot plot that shows the variation of cost within the top 5 Products
+
+ggplot(top_5_products, aes(ScrapedIndexPrice, ProductName)) +
+  geom_point(aes(color = ScrapedIndexVendor)) +
+  scale_x_continuous(breaks = seq(0, 75, 5)) +
+  xlab("Scraped Index Price") +
+  ylab("Product Name")
