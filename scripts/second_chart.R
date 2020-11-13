ac712aeea397798a693be6dc92026c0178cc53d4
Yutian Pan <yupan@uw.edu>
2020-11-13 02:21:24 -0700

    decide on question and chart

---
+++ scripts/second_chart.R
@@ -0,0 +1,30 @@
+#main product listing page 
+#subsequent listing page 
+
+#items lacked a shipping price 
+#(sold by amazon or sellers in "fullfilled by amazon")
+
+# 1st ranking, just the price (without shipping)
+# 2nd ranking, re-ranking after adding shipping. 
+
+# buy box (BB)
+# pop up first as a suggesed purchase 
+# 3/4 amazon own product / seller "fullfilled by amazon"
+# even when much cheaper options. 
+
+# amazon's giving ranking: 
+# all vendors  price + shipping
+# BUT own/FBA product OMIT shipping when ranking
+
+# To: show the different the price of one product (scraped/corrected/ )?
+
+# To: highest price - lowest price (box-plot)? 
+
+
+
+# How likely BBCorrectedPrice is (not) the lower price, based on different vendor type?
+
+# Faceted histogram:
+# three categories by “ScrapedIndexVendortype” : Amazon, FBA, Other. 
+# showing the distribution of “CorrectedPrice” / ”BBCorrectedPrice” ratio of all vendor listings.
+# If “CorrectedPrice”/”BBCorrectedPrice” Ratio < 1, BBCorrectedPrice is not the lower price in the listing.
