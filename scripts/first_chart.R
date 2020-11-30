07078ee96ecd5b1f4ea290fb7fc49bd6382e459e
Sydney <smiths12@uw.edu>
2020-11-29 21:50:06 -0800

    Chart 1 fixed

--- scripts/first_chart.R
+++ scripts/first_chart.R
@@ -2,14 +2,7 @@ library(tidyverse)
 
 # Load the data from GitHub
 
-amazon_data <- read.csv(
-  paste("https://raw.githubusercontent.com/info201b-au20/project-",
-    "AmazonPricing/gh-pages/data/Amazon-Ranking-Analysis.csv?token=",
-    "AKQ3B6Q3UEQDBPM4IBQS3B27W2R5W",
-    sep = ""
-  )
-)
-
+amazon_data <- read.csv("https://raw.githubusercontent.com/info201b-au20/project-AmazonPricing/gh-pages/data/Amazon-Ranking-Analysis.csv?token=ARIOKKKAAFIU5IRJ5ZKCJT27ZW5S2")
 # Check the number of total unique products is 250
 check <- amazon_data %>%
   group_by(ProductName) %>%
@@ -17,7 +10,7 @@ check <- amazon_data %>%
 
 # We will be examining the top 5 unique products which means the top 272 Indexes
 top_5_products <- amazon_data %>%
-  filter(Index <= 44)
+  filter(Index <= 44) %>% mutate(ProductName = strtrim(ProductName,51))
 
 # Create a dot plot that shows the variation of cost within the top 5 Products
 
