47a2b384339a5625e4905d965bcd067ac361cb64
alexismacaskilll <alexismacaskill2@gmail.com>
2020-12-07 22:41:04 -0800

    fix pie

--- app_ui.R
+++ app_ui.R
@@ -4,40 +4,25 @@ library(shiny)
 library(stringr)
 source("app_server.R")
 
-########################## PIE CHART (third chart) ###########################
+############################ START INTRO PANEL ##############################
+############################# END INTRO PANEL ###############################
+
+############################ START FIRST CHART ##############################
+############################# END FIRST CHART ###############################
+
+############################ START SECOND CHART ##############################
+############################# END SECOND CHART ###############################
+
+############################ START THIRD CHART ##############################
 pie_sidebar_content <- 
   sliderInput("indices", "Indices to include",
-              min = 1, max = 250, value = c(1, 250),
+              min = 1, max = 162, value = c(1, 162),
               width = '500px'
   )
 
 pie_main_content <- mainPanel(
   plotlyOutput("pie"), 
-  pie_sidebar_content,
-  tags$p(
-    paste(
-      "The values that I calculated in the Summary tab found that",
-      "in 2015, on average, people in the United States emitted",
-      "about 3.5 times as much CO₂ as other people in the world. I",
-      "wanted to investigate how the CO₂ Per Capita has increased overtime", 
-      "in the United States. This vizualization not only lets you do that,",
-      "but it also allows you to look at the annual CO₂ emmissions overtime.",
-      "In addition, the vizualization lets you choose any country or the",
-      "entire world. This can help us identify trends like how has the annual",
-      "CO₂ emissions changed for different countries overtime, and how the",
-      "CO₂ emissions per capita has changed for different countries overtime.",
-      "I observed that for the United States, Annual CO₂ emissions started",
-      "increasing at a faster rate around 1900, but since 2007 they actually",
-      "have been decreasing. This might be because of the reduction in the",
-      "use of coal for power generation in the United States. Another reason",
-      "could be that in 2016, the United States became a signatory to the",
-      "Paris Agreement, an agreement within the United Nations Framework",
-      "Convention on Climate Change. The world's annual CO₂ emissions has",
-      "been rapidly increasing since around 1950, but it hasn't really had",
-      "any significant decreases that are visible.",
-      sep = " "
-    )
-  )
+  pie_sidebar_content
 )
 
 third_chart_panel <- tabPanel(
@@ -46,16 +31,38 @@ third_chart_panel <- tabPanel(
   titlePanel("Percentage of each Vendor Type"),
   pie_main_content
 )
-####################### END OF PIE CHART (third chart) ########################
+############################# END THIRD CHART ###############################
+
+############################ START SUMMARY PANEL ##############################
+summary_main_content <- mainPanel(
+  tags$p(
+    paste(
+      "This is just random text now but we should fill it in with",
+      "observations from the three charts above (one from each).",
+      sep = " "
+    )
+  )
+)
+
+summary_panel <- tabPanel(
+  "Summary Takeaways",
+  # Add a titlePanel to your tab
+  titlePanel("Takeaways from our Analysis"),
+  summary_main_content
+)
+
+############################# END SUMMARY PANEL ###############################
+
+
 
 
 # Define a variable `ui` storing a `navbarPage()` element containing
 # your the pages defined above
 ui <- navbarPage(
   "Amazon Pricing",
-  # summary_panel,
+  # introduction_panel,
   # first_chart_panel, 
   # second_chart_panel, 
-  third_chart_panel 
-  # conclusion_panel
+  third_chart_panel,
+  summary_panel
 )
