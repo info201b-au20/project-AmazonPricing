dcc784ac592861efc0cdf3b67264ccc4f4dcf139
Sydney <smiths12@uw.edu>
2020-12-08 16:10:13 -0800

    First Chart Complete

--- app_server.R
+++ app_server.R
@@ -7,6 +7,8 @@ library(plotly)
 # Read in data
 amazon_data <- read.csv("./data/Amazon-Ranking-Analysis.csv", stringsAsFactors = FALSE)
 
+product <- unique(amazon_data$ProductName)
+
 source("./scripts/build_pie.R")
 
 # Start shinyServer
@@ -14,5 +16,12 @@ server <- function(input, output) {
   # Render a plotly object that returns your scatter plot
   output$pie <- renderPlotly({
     return(build_pie(amazon_data, input$indices))
+  })
+  output$price_chart <- renderPlotly({
+    plot <- ggplot(data = amazon_data %>% filter(ProductName == input$product_name)) +
+      geom_point(mapping = aes(x = ScrapedIndexPrice, y = ScrapedIndexVendor),
+                 color = input$color_input) + xlab("Price ($)") + ylab("Vendor")
+    
+    ggplotly(plot)
   })
 }
